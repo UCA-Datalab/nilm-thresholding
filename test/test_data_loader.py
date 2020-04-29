6eabfe6e9b67994161633a5b3182624a86211693
Daniel Precioso <danipg1409@gmail.com>
2020-04-29 17:48:27 +0200

    Updated test

--- test/test_data_loader.py
+++ test/test_data_loader.py
@@ -1,7 +1,10 @@
+import sys
+sys.path.insert(0, "../better_nilm")
 from better_nilm.nilmtk.data_loader import building_metergroup_from_file
 
-path_file = "../data/nilmtk/redd.h5"
+path_file = "../nilm/data/nilmtk/redd.h5"
 building = 1
-appliances = ["dishwasher"]
+appliances = ["dishwasher", "microwave"]
 
 metergroup = building_metergroup_from_file(path_file, building, appliances)
+print(metergroup)
