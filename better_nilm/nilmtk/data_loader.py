f0632278ceed86326196c261fad5da2c2dcb7ec7
Daniel Precioso <danipg1409@gmail.com>
2020-04-29 18:41:01 +0200

    Docstring and errors

--- better_nilm/nilmtk/data_loader.py
+++ better_nilm/nilmtk/data_loader.py
@@ -1,22 +1,27 @@
 from nilmtk import DataSet
+from nilmtk.metergroup import MeterGroup
 
+from better_nilm.format_utils import to_list
 from better_nilm.format_utils import to_tuple
 
 
-def building_metergroup_from_file(path_file, building, appliances):
+def metergroup_from_file(path_file, building, appliances=None):
     """
+    Opens given h5 file (preprocessed by nilmtk library), goes to target
+    building and outputs every electric meter related to the target appliances.
+
     Params
     ------
     path_file : str
         Path to the h5 file containing a dataset processed by nilmtk
     building : int
         Building ID to read
-    appliances : list
-        Appliances to extract
+    appliances : list, default=None
+        Appliances to extract. If None, extract all appliances
 
     Returns
     -------
-    elec_meters : nilmtk.Metergroup
+    metergroup : nilmtk.metergroup.Metergroup
         List of electric meters of target appliances in target building,
         also including the main (whole house) meter
     """
@@ -30,23 +35,24 @@ def building_metergroup_from_file(path_file, building, appliances):
     # Load meter records
     elec = data.buildings.get(building).elec
 
-    # Check which target appliances are in the building
-    building_appliances = elec.label().split(", ")
-    building_appliances = set([app.lower() for app in building_appliances])
     if appliances is None:
         # Take all appliances
         return elec
-    else:
-        # Remove from list the appliances not in the building
-        target_appliances = [app for app in appliances if
-                             app in building_appliances]
 
-    # If there are no target appliances, end function and return None
+    # Check which target appliances are in the building
+    building_appliances = elec.label().split(", ")
+    building_appliances = set([app.lower() for app in building_appliances])
+    # Remove from list the appliances not in the building
+    target_appliances = [app for app in to_list(appliances) if
+                         app in building_appliances]
+
+    # If there are no target appliances, raise error
     if len(target_appliances) == 0:
-        print(
-            f"No target appliance found in house {building} of {path_file}")
-        return None
-    # If there is any target appliance, we keep going
+        raise ValueError(f"None of the target appliances found in house"
+                         f" {building} of {path_file}\n"
+                         f"Target appliances: {', '.join(appliances)}\n"
+                         "Building appliances: "
+                         f"{', '.join(building_appliances)}")
 
     # Total electric load (aggregated)
     elec_main = to_tuple(elec.mains().instance())
@@ -62,5 +68,46 @@ def building_metergroup_from_file(path_file, building, appliances):
     elec_instances = list(elec_int + elec_tuples)
 
     # Take all relevant meters
-    elec_meters = elec.select(instance=elec_instances)
-    return elec_meters
+    metergroup = elec.select(instance=elec_instances)
+    return metergroup
+
+
+def metergroup_to_array(metergroup, appliances=None, sample_period=6,
+                        window_size=600, max_windows=None):
+    """
+
+    Params
+    ------
+    metergroup : nilmtk.metergroup.Metergroup
+        List of electric meters, including the main meters of the house
+    appliances : list, default=None
+        List of appliances to include in the array. They don't need
+        to be in the metergroup - in those cases, we assume that the
+        appliances are always off (load 0). If None, take all the
+        appliances in the metergroup.
+    sample_period : int, default=6
+        Time between consecutive electric load records, in seconds.
+        By default we take 6 seconds.
+    window_size : int, default=600
+        Number of consecutive records to take at once. By default is 600,
+        which implies that a default time windows comprehends one hour
+        worth of records (600 records x 6 seconds between each).
+    max_windows : int, default=None
+        Maximum number of windows to output.
+
+    Returns
+    -------
+    ser : numpy.array
+        shape = (meters, window_size, windows)
+        - meters : The number of appliances, plus the main meter.
+            They are sorted alphabetically by appliance name, excluding
+            the main meter, which always goes first.
+        - window_size : see Params
+        - windows : The amount of windows that could be extracted from the
+            metergroup.
+
+    """
+    assert type(metergroup) is MeterGroup, f"metergroup param must be type " \
+                                           f"nilmtk.metergroup.MeterGroup\n" \
+                                           f"Provided param is type " \
+                                           f"{type(metergroup)}"
--- test/test_data_loader.py
+++ test/test_data_loader.py
@@ -1,10 +1,13 @@
 import sys
 sys.path.insert(0, "../better_nilm")
-from better_nilm.nilmtk.data_loader import building_metergroup_from_file
+
+from better_nilm.nilmtk.data_loader import metergroup_from_file
+from better_nilm.nilmtk.data_loader import metergroup_to_array
 
 path_file = "../nilm/data/nilmtk/redd.h5"
-building = 1
+building = 5
 appliances = ["dishwasher", "microwave"]
 
-metergroup = building_metergroup_from_file(path_file, building, appliances)
-print(metergroup)
+metergroup = metergroup_from_file(path_file, building, appliances=appliances)
+metergroup_to_array(metergroup)
+
