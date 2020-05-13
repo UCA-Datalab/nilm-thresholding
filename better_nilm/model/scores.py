33625efe6b1c75347cdef96e3d4c20d23832c4d9
Daniel Precioso <danipg1409@gmail.com>
2020-05-13 11:38:31 +0200

    mse to rmse

--- better_nilm/model/scores.py
+++ better_nilm/model/scores.py
@@ -8,15 +8,16 @@ def rmse_score(pred, real):
                                      "have the same shape."
 
     num_appliances = pred.shape[2]
-    rmse = np.zeros(num_appliances)
+    mse = np.zeros(num_appliances)
 
     for idx in range(num_appliances):
         p = pred[:, :, idx].copy()
         p = p.flatten()
         r = real[:, :, idx].copy()
         r = r.flatten()
-        rmse[idx] = mean_squared_error(p, r)
+        mse[idx] = mean_squared_error(p, r)
 
+    rmse = np.sqrt(mse)
     return rmse
 
 
