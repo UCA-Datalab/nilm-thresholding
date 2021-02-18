ee360c5e1bd692111a28fedc5bf52bc0aa08ae81
Daniel Precioso <danipg1409@gmail.com>
2021-02-18 11:46:05 +0100

    Update train_test_sequential

--- train_test_sequential.sh
+++ train_test_sequential.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+rm -r ./configs
 python better_nilm/generate_config_files.py
 FILES=./configs/*
 for f in $FILES
 do
   echo "Processing $f file..."
   # take action on each file. $f store current file name
-  python better_nilm/train_test_model.py --path-config $f
+  python better_nilm/train_model.py --path-config $f
+  python better_nilm/test_model.py --path-config $f
 done
