6cccfab2044df491148ba06b86379b8f49375c5e
daniprec <danipg1409@gmail.com>
2021-04-14 19:06:14 +0200

    Move dataloader generators to wrapper

--- nilm_thresholding/data/wrapper.py
+++ nilm_thresholding/data/wrapper.py
@@ -1,11 +1,64 @@
+import numpy as np
+import torch
+import torch.utils.data as data
+from torch.utils.data import DataLoader
+
 from nilm_thresholding.utils.format_list import to_list
 from nilm_thresholding.utils.threshold import get_threshold_params
+
+
+class Power(data.Dataset):
+    def __init__(
+        self,
+        meter=None,
+        appliance=None,
+        status=None,
+        length=512,
+        border=16,
+        power_scale=2000.0,
+        train=False,
+    ):
+        self.length = length
+        self.border = border
+        self.power_scale = power_scale
+        self.train = train
+
+        self.meter = meter.copy() / self.power_scale
+        self.appliance = appliance.copy() / self.power_scale
+        self.status = status.copy()
+
+        self.epochs = (len(self.meter) - 2 * self.border) // self.length
+
+    def __getitem__(self, index):
+        i = index * self.length + self.border
+        if self.train:
+            i = np.random.randint(
+                self.border, len(self.meter) - self.length - self.border
+            )
+
+        x = self.meter.iloc[
+            i - self.border : i + self.length + self.border
+        ].values.astype("float32")
+        y = self.appliance.iloc[i : i + self.length].values.astype("float32")
+        s = self.status.iloc[i : i + self.length].values.astype("float32")
+        x -= x.mean()
+
+        return x, y, s
+
+    def __len__(self):
+        return self.epochs
 
 
 class DataloaderWrapper:
     build_idx_train: list = list()
     build_idx_valid: list = list()
     build_idx_test: list = list()
+    list_series_meter: list = []
+    list_series_appliance: list = []
+    list_series_status: list = []
+    dl_train: DataLoader = None
+    dl_valid: DataLoader = None
+    dl_test: DataLoader = None
 
     def __init__(self, config: dict):
         # Read parameters from config files
@@ -82,3 +135,95 @@ class DataloaderWrapper:
         assert (
             len(self.build_idx_test) > 0
         ), f"No ID in build_id_test matches the ones of buildings."
+
+    def _train_valid_test(self):
+        """
+        Splits lists of pandas.Series data into train, validation and tests.
+        """
+        df_len = [len(self.list_series_meter[i]) for i in range(self.num_buildings)]
+
+        ds_train = [
+            Power(
+                self.list_series_meter[i][: int(self.train_size * df_len[i])],
+                self.list_series_appliance[i][: int(self.train_size * df_len[i])],
+                self.list_series_status[i][: int(self.train_size * df_len[i])],
+                self.output_len,
+                self.border,
+                self.power_scale,
+                train=True,
+            )
+            for i in range(self.num_buildings)
+        ]
+
+        ds_valid = [
+            Power(
+                self.list_series_meter[i][
+                    int(self.train_size * df_len[i]) : int(
+                        (self.train_size + self.valid_size) * df_len[i]
+                    )
+                ],
+                self.list_series_appliance[i][
+                    int(self.train_size * df_len[i]) : int(
+                        (self.train_size + self.valid_size) * df_len[i]
+                    )
+                ],
+                self.list_series_status[i][
+                    int(self.train_size * df_len[i]) : int(
+                        (self.train_size + self.valid_size) * df_len[i]
+                    )
+                ],
+                self.output_len,
+                self.border,
+                self.power_scale,
+                train=False,
+            )
+            for i in range(self.num_buildings)
+        ]
+
+        ds_test = [
+            Power(
+                self.list_series_meter[i][
+                    int((self.train_size + self.valid_size) * df_len[i]) :
+                ],
+                self.list_series_appliance[i][
+                    int((self.train_size + self.valid_size) * df_len[i]) :
+                ],
+                self.list_series_status[i][
+                    int((self.train_size + self.valid_size) * df_len[i]) :
+                ],
+                self.output_len,
+                self.border,
+                self.power_scale,
+                train=False,
+            )
+            for i in range(self.num_buildings)
+        ]
+
+        return ds_train, ds_valid, ds_test
+
+    def _list_series_to_dataloader(self, list_series, build_idx, shuffle):
+        """
+        Turns a list of pandas.Series into a dataloader.
+        """
+        ds = []
+        for idx in build_idx:
+            ds += [list_series[idx]]
+        ds = torch.utils.data.ConcatDataset(ds)
+        dl = DataLoader(dataset=ds, batch_size=self.batch_size, shuffle=shuffle)
+        return dl
+
+    def _list_series_to_dataloaders(self):
+        """
+        Turns list of pandas.Series into dataloaders.
+        """
+        ds_train, ds_valid, ds_test = self._train_valid_test()
+
+        self.dl_train = self._list_series_to_dataloader(
+            ds_train, self.build_idx_train, True
+        )
+        self.dl_valid = self._list_series_to_dataloader(
+            ds_valid, self.build_idx_valid, False
+        )
+        self.dl_test = self._list_series_to_dataloader(
+            ds_test, self.build_idx_test, False
+        )
