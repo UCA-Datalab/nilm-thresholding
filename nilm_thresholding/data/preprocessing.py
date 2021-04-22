1d88eee81e9fb5625ca83e3d3b2e748cd47ca5c2
daniprec <danipg1409@gmail.com>
2021-04-22 14:08:12 +0200

    Avoid thresholding in preprocessing

--- nilm_thresholding/data/preprocessing.py
+++ nilm_thresholding/data/preprocessing.py
@@ -1,10 +1,8 @@
 import logging
 import os
 
-import numpy as np
 import pandas as pd
 
-from nilm_thresholding.data.threshold import Threshold
 from nilm_thresholding.utils.format_list import to_list
 
 logging.basicConfig(level=logging.DEBUG, format="%(message)s")
@@ -20,95 +18,39 @@ class PreprocessWrapper:
         dates: dict = None,
         period: str = "1min",
         train_size: float = 0.8,
-        valid_size: float = 0.1,
         input_len: int = 510,
         border: int = 15,
         max_power: float = 10000,
-        threshold: dict = None,
         **kwargs,
     ):
         # Read parameters from config files
-        self.appliances = [] if appliances is None else to_list(appliances)
+        self.appliances = [] if appliances is None else sorted(to_list(appliances))
         self.buildings = [] if buildings is None else buildings[self.dataset]
         self.dates = [] if dates is None else dates[self.dataset]
         self.period = period
-        self.size = {
-            "train": train_size,
-            "validation": valid_size,
-            "test": 1 - train_size - valid_size,
-        }
+        self.train_size = train_size
         self.input_len = input_len
         self.border = border
+        self.step = self.input_len - self.border
         self.max_power = max_power
 
-        # Set the parameters according to given threshold method
-        param_thresh = {} if threshold is None else threshold
-        self.threshold = Threshold(appliances=self.appliances, **param_thresh)
-
         logging.debug(
-            f"Received extra kwargs, not used:\n   {', '.join(kwargs.keys())}"
+            f"Preprocessing received extra kwargs, not used:\n"
+            f"    {', '.join(kwargs.keys())}\n"
         )
 
     def load_house_meters(self, house: int) -> pd.DataFrame:
         """Placeholder function, this should load the household meters and status"""
         return pd.DataFrame()
 
-    def compute_thresholds(self, buildings: list = None):
-        """Compute the thresholds of each appliance, using the given list of buildings"""
-        buildings = self.buildings if buildings is None else buildings
-        for app in self.appliances:
-            ser = [0] * len(buildings)
-            idx = 0
-            for house in buildings:
-                # Load the chosen meters of the building, compute their status
-                meters = self.load_house_meters(house)
-                ser[idx] = meters[app].values
-                idx += 1
-            ser = np.concatenate(ser)
-            self.threshold.update_appliance_threshold(ser, app)
-
-    def _include_status(self, meters: pd.DataFrame):
-        """Includes the status columns for each device
-
-        Parameters
-        ----------
-        meters : pandas.DataFrame
-
-        Returns
-        -------
-        pandas.DataFrame
-            Same dataframe, with status columns
-
-        """
-        ser = meters.drop("aggregate", axis=1).values
-        status = self.threshold.get_status(ser)
-        status = pd.DataFrame(status, columns=self.appliances, index=meters.index)
-        meters = meters.merge(
-            status,
-            how="inner",
-            on=None,
-            left_on=None,
-            right_on=None,
-            left_index=True,
-            right_index=True,
-            sort=False,
-            suffixes=("", "_status"),
-            copy=True,
-            indicator=False,
-            validate=None,
-        )
-        return meters
-
     def store_preprocessed_data(self, path_output: str):
         """Stores preprocessed data in output folder"""
-        self.compute_thresholds(buildings=[1])
         # Loop through the buildings that are going to be stored
         for house in self.buildings:
             # Load the chosen meters of the building, compute their status
             meters = self.load_house_meters(house)
             # Check the number of data points
-            step = self.input_len - self.border
-            size = meters.shape[0] // step
+            size = meters.shape[0] // self.step
             idx = 0
             # Store data points sequentially
             # Create the building folder inside each subset folder
@@ -128,4 +70,4 @@ class PreprocessWrapper:
                 # Sort columns by name
                 df_sub = df_sub.reindex(sorted(df_sub.columns), axis=1)
                 df_sub.to_csv(path_file)
-                idx += step
+                idx += self.step
