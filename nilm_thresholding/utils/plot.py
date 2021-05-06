89f06a59bc431e09dd5d4a2265fd2e0a480b1da9
Daniel Precioso <danipg1409@gmail.com>
2021-05-06 14:43:06 +0200

    Add plot function: real vs prediction

--- nilm_thresholding/utils/plot.py
+++ nilm_thresholding/utils/plot.py
@@ -19,10 +19,44 @@ def plot_real_data(
     # List appliances
     list_app = list(dict_pred.keys())
     list_app.remove("aggregated")
+    # Plot each appliance
     for app in list_app:
         name = app.capitalize().replace("_", " ")
         y = dict_pred[app]["power"][idx_start:idx_end]
         ax.plot(x, y, label=name)
+    # Add plot information
+    ax.legend()
+    ax.set_ylabel("Power (watts)")
+    ax.set_xlabel("Time (minutes)")
+    ax.grid()
+    if savefig is None:
+        return fig, ax
+    else:
+        plt.savefig(savefig)
+        plt.close()
+
+
+def plot_real_vs_prediction(
+    dict_pred: dict,
+    appliance: str,
+    figsize: tuple = (10, 6),
+    idx_start: int = 0,
+    idx_end: int = 480,
+    freq_min: float = 1,
+    savefig: str = None,
+):
+    """Plot the real and predicted power consumption of an appliance"""
+    fig, ax = plt.subplots(figsize=figsize)
+    # Plot real power
+    y = dict_pred[appliance]["power"][idx_start:idx_end]
+    x = np.arange(0, len(y)) * freq_min
+    ax.plot(x, y, label="Real")
+    # Plot prediction
+    y = dict_pred[appliance]["power_pred"][idx_start:idx_end]
+    ax.plot(x, y, label="Prediction")
+    # Add plot information
+    name = appliance.capitalize().replace("_", " ")
+    ax.set_title(name)
     ax.legend()
     ax.set_ylabel("Power (watts)")
     ax.set_xlabel("Time (minutes)")
