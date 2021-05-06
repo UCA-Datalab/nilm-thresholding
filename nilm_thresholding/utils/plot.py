a9a5a48d9e3627123745ab31e088cd82d25af43c
Daniel Precioso <danipg1409@gmail.com>
2021-05-06 14:28:11 +0200

    Update plot function to allow frequency change

--- nilm_thresholding/utils/plot.py
+++ nilm_thresholding/utils/plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_real_data(
@@ -6,22 +7,28 @@ def plot_real_data(
     figsize: tuple = (10, 6),
     idx_start: int = 0,
     idx_end: int = 480,
+    freq_min: float = 1,
     savefig: str = None,
 ):
     """Plot the aggregated power load and each appliances' consumption"""
+    fig, ax = plt.subplots(figsize=figsize)
+    # Plot aggregate power
+    y = dict_pred["aggregated"][idx_start:idx_end]
+    x = np.arange(0, len(y)) * freq_min
+    ax.plot(x, y, label="Aggregate")
     # List appliances
     list_app = list(dict_pred.keys())
-    list_app.remove("aggregate")
-    fig = plt.figure(figsize=figsize)
-    axs = fig.add_axes()
-    axs.plot(dict_pred["aggregate"][idx_start:idx_end], label="Aggregate")
+    list_app.remove("aggregated")
     for app in list_app:
         name = app.capitalize().replace("_", " ")
-        axs.plot(dict_pred[app]["power"][idx_start:idx_end], label=name)
-    axs.legend()
-    axs.set_ylabel("Power (watts)")
+        y = dict_pred[app]["power"][idx_start:idx_end]
+        ax.plot(x, y, label=name)
+    ax.legend()
+    ax.set_ylabel("Power (watts)")
+    ax.set_xlabel("Time (minutes)")
+    ax.grid()
     if savefig is None:
-        return fig, axs
+        return fig, ax
     else:
         plt.savefig(savefig)
         plt.close()
