489a566542dd775710da1a1c9ad02e41ce2c6386
Daniel Precioso <danipg1409@gmail.com>
2021-06-01 17:53:33 +0200

    Method to list files and metadata

--- nilmth/data/dataport.py
+++ nilmth/data/dataport.py
@@ -11,17 +11,21 @@ class Dataport(Preprocessing):
     dataset: str = "dataport"
     datastore: HDFStore = None
     _dict_houses: dict = {}
+    _list_path_files: list = []
+    _list_path_metadata: list = []
 
     def __init__(self, path_data: str, **kwargs):
         super(Dataport, self).__init__(**kwargs)
         self._path_data = path_data
+        # List paths and locate houses
+        self._list_paths()
         self._locate_houses()
 
-    @property
-    def _list_path_files(self):
-        """List of data files"""
+    def _list_paths(self):
+        """List all paths to data files and metadata"""
+        list_path_files = []
+        list_path_metadata = []
         # List data files
-        path_files = []
         for path in os.listdir(self._path_data):
             path_subset = os.path.join(self._path_data, path)
             files = [
@@ -29,8 +33,15 @@ class Dataport(Preprocessing):
                 for f in os.listdir(path_subset)
                 if f.endswith(".csv") and not f.startswith("meta")
             ]
-            path_files += files
-        return sorted(path_files)
+            list_path_files += files
+            metadata = [
+                os.path.join(path_subset, f)
+                for f in os.listdir(path_subset)
+                if f.endswith(".csv") and f.startswith("meta")
+            ]
+            list_path_metadata += metadata
+        self._list_path_files = sorted(list_path_files)
+        self._list_path_metadata = sorted(list_path_metadata)
 
     def _locate_houses(self):
         """Updates the _dict_houses dictionary, listing the file where we can find
