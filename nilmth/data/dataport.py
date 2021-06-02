0cf2ccf4cc5a3d9878fd7c9c1d5001dff2e64142
daniprec <danipg1409@gmail.com>
2021-06-02 13:36:20 +0200

    Dataport class gets the metadata of each house

--- nilmth/data/dataport.py
+++ nilmth/data/dataport.py
@@ -5,11 +5,13 @@ import pandas as pd
 from pandas.io.pytables import HDFStore
 
 from nilmth.data.preprocessing import Preprocessing
+from nilmth.utils.logging import logger
 
 
 class Dataport(Preprocessing):
     dataset: str = "dataport"
     datastore: HDFStore = None
+    metadata: pd.DataFrame = None
     _dict_houses: dict = {}
     _list_path_files: list = []
     _list_path_metadata: list = []
@@ -20,9 +22,11 @@ class Dataport(Preprocessing):
         # List paths and locate houses
         self._list_paths()
         self._locate_houses()
+        self._read_metadata()
 
     def _list_paths(self):
         """List all paths to data files and metadata"""
+        logger.debug(f"Listing data and metadata files in {self._path_data}")
         list_path_files = []
         list_path_metadata = []
         # List data files
@@ -42,15 +46,57 @@ class Dataport(Preprocessing):
             list_path_metadata += metadata
         self._list_path_files = sorted(list_path_files)
         self._list_path_metadata = sorted(list_path_metadata)
+        logger.debug("Done listing files")
 
     def _locate_houses(self):
         """Updates the _dict_houses dictionary, listing the file where we can find
         each house"""
+        logger.debug("Listing the available houses IDs")
         for path_file in self._list_path_files:
             df = pd.read_csv(path_file, usecols=["dataid"])
             self._dict_houses.update(
                 dict(zip(df["dataid"].unique(), itertools.repeat(path_file)))
             )
+        logger.debug(f"Found {len(self._dict_houses)} available houses")
+
+    def _read_metadata(self):
+        """Reads all metadata files and stores them in metadata attribute"""
+        logger.debug("Reading all the metadata")
+        # List houses IDs
+        list_dataid = [str(x) for x in self._dict_houses.keys()]
+        # Initialize list of dataframes
+        list_df = [pd.DataFrame()] * len(self._list_path_metadata)
+        # Loop through all the metadata files
+        for i, path_meta in enumerate(self._list_path_metadata):
+            df = pd.read_csv(
+                path_meta,
+                parse_dates=[
+                    "egauge_1min_min_time",
+                    "egauge_1min_max_time",
+                    "egauge_1s_min_time",
+                    "egauge_1s_max_time",
+                    "date_enrolled",
+                    "date_withdrawn",
+                ],
+            ).query("dataid in @list_dataid")
+            list_df[i] = df
+        # Concatenate all metadata dataframes and do a bit of cleaning
+        self.metadata = (
+            pd.concat(list_df)
+            .sort_values("dataid")
+            .drop_duplicates()
+            .reset_index(drop=True)
+            .astype({"dataid": int})
+            .replace("yes", True)
+        )
+        logger.debug("Done reading the metadata")
+
+    def get_metadata(self, house: int):
+        """Returns the metadata of a house given its ID"""
+        dict_house = self.metadata.query(f"dataid == {house}").to_dict("r")[0]
+        # Include path to file
+        dict_house.update({"path": self._dict_houses[house]})
+        return dict_house
 
     def load_house_meters(self, house: int) -> pd.DataFrame:
         pass
